1599874e6649857cb22def1a7caa024f4c8ef34d
amulyad95 <33649134+amulyad95@users.noreply.github.com>
2021-09-29 10:58:46 +0530

    Delete JavaFullStack_BED2_Quiz(Tanmay_Tiwari_20127725)/OnlineQuiz directory

--- JavaFullStack_BED2_Quiz(Tanmay_Tiwari_20127725)/OnlineQuiz/src/main/java/com/BED2/OnlineQuiz/SpringData/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.BED2.OnlineQuiz.SpringData;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.BED2.OnlineQuiz.Pojo.TestDetails;
-
-public interface TestRepository extends CrudRepository<TestDetails,String> {
-
-}
